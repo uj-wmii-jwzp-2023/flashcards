200a7a9f26bfd83476be7ea53caa44ef55011268
ignacy.alwasiak@gmail.com <ignacy.alwasiak@gmail.com>
2023-05-27 22:05:27 +0200

    added tests for login and user creation

--- src/test/java/wmii/jwzp/flashcards/controller/UserControllerTests.java
+++ src/test/java/wmii/jwzp/flashcards/controller/UserControllerTests.java
@@ -1,58 +1,110 @@
 package wmii.jwzp.flashcards.controller;
 
-import wmii.jwzp.flashcards.utils.JSON;
+import wmii.jwzp.flashcards.model.db.SessionModel;
+import wmii.jwzp.flashcards.model.db.UserModel;
+import wmii.jwzp.flashcards.repository.SessionRepository;
+import wmii.jwzp.flashcards.repository.UserRepository;
+import wmii.jwzp.flashcards.utils.PasswordHash;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
+import jakarta.servlet.http.Cookie;
 
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @AutoConfigureDataJpa
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UsersControllerTests {
 
   @Autowired
   UserController userController;
 
+
+  @MockBean
+  private UserRepository userRepository;
+
+  @MockBean
+  private SessionRepository sessionRepository;
+
   @Autowired
   private MockMvc api;
 
   @Test
-  void shouldReturnAllUsers() throws Exception {
-    api.perform(get("/users"))
-        .andExpect(status().isOk());
+  void shouldRegisterNewUser() throws Exception {
+    Mockito.when(userRepository.existsByNick(anyString())).thenReturn(false);
+    api.perform(post("/users")
+      .contentType("application/json")
+      .content("{\"nick\": \"ok2\",\"password\": \"ok\"}"))
+      .andExpect(status().isOk());
+  }
+  @Test
+  void userExistsCreationFails() throws Exception {
+    Mockito.when(userRepository.existsByNick(anyString())).thenReturn(true);
+    api.perform(post("/users")
+      .contentType("application/json")
+      .content("{\"nick\": \"ok2\",\"password\": \"ok\"}"))
+      .andExpect(status().is4xxClientError());
+  }
+  @Test
+  void loginUser() throws Exception {
+    UserModel retuserModel = new UserModel();
+    retuserModel.setHashedPassword(PasswordHash.hash("ok2"));
+    Mockito.when(userRepository.findByNick(anyString())).thenReturn(retuserModel);
+    
+    api.perform(post("/users/login")
+      .header("Authorization", "Basic b2s6b2sy"))//login: ok password: ok2
+      .andExpect(status().isOk());
+  }
+  
+  
+  @Test
+  void failLoginWrongCredentials() throws Exception {
+    UserModel retuserModel = new UserModel();
+    retuserModel.setHashedPassword(PasswordHash.hash("wrong_password"));
+    Mockito.when(userRepository.findByNick(anyString())).thenReturn(retuserModel);
+    
+    api.perform(post("/users/login")
+      .header("Authorization", "Basic b2s6b2sy"))//login: ok password: ok2
+      .andExpect(status().is4xxClientError());
+    }
+    
+  @Test
+  void refresSuccess() throws Exception {
+    SessionModel sessionModel = new SessionModel();
+    sessionModel.setCreatedAt();
+    sessionModel.setExpiresAt();
+    Optional<SessionModel> optionalSessionModel = Optional.of(sessionModel);
+    Mockito.when(sessionRepository.findById(anyString())).thenReturn(optionalSessionModel);
+    
+    api.perform(post("/users/session")
+      .cookie(new Cookie("sid","123")))
+      .andExpect(status().isOk());
   }
 
   @Test
-  void shouldRegisterNewUser() throws Exception {
-    var json = JSON.stringify(Map.of(
-        "mail", "sample@test.com",
-        "name", "tom",
-        "password", "123"));
-
-    api.perform(post("/users")
-        .content(json)
-        .header(CONTENT_TYPE, APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.id").exists())
-        .andExpect(jsonPath("$.id").isNotEmpty());
+  void refresSuccessFailNoSession() throws Exception {
+    Optional<SessionModel> optionalSessionModel = Optional.empty();
+    Mockito.when(sessionRepository.findById(anyString())).thenReturn(optionalSessionModel);
+    
+    api.perform(post("/users/session")
+      .cookie(new Cookie("sid","123")))
+      .andExpect(status().is4xxClientError());
   }
+  
 }
